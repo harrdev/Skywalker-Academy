010f078f4facbce8715204213062c643f08d0192
harrdev <jasonharrphx@live.com>
2021-11-13 19:06:41 -0700

    people now displaying more than 1 page at a time from API req

--- controllers/swPeople.js
+++ controllers/swPeople.js
@@ -3,14 +3,36 @@ const router = express.Router()
 const axios = require('axios')
 
 // INDEX ROUTE
+// router.get('/', (req, res) => {
+//     let swapi = 'https://swapi.dev/api/people'
+//     // Use request to call the API
+//     axios.get(swapi).then(apiResponse => {
+//       let swPeople = apiResponse.data.results;
+//       res.render('people.ejs', { swPeople })
+//     })
+//   });
+
+let one = "https://swapi.dev/api/people"
+let two = "https://swapi.dev/api/people/?page=2"
+let three = "https://swapi.dev/api/people/?page=3"
+
+const requestOne = axios.get(one);
+const requestTwo = axios.get(two);
+const requestThree = axios.get(three);
+
 router.get('/', (req, res) => {
-    let swapi = 'https://swapi.dev/api/people';
-    // Use request to call the API
-    axios.get(swapi).then(apiResponse => {
-      let swPeople = apiResponse.data.results;
-      console.log("these are the people: ", swPeople)
-      res.render('people.ejs', { swPeople })
-    })
-  });
+    axios.all([requestOne, requestTwo, requestThree]).then(axios.spread((...responses) => {
+        const responseOne = responses[0]
+        const responseTwo = responses[1]
+        const responseThree = responses[2]
+        // use/access the results 
+        let swPeople = responseOne.data.results
+        let swPeople2 = responseTwo.data.results
+        let swPeople3 = responseThree.data.results
+        res.render('people.ejs', { swPeople, swPeople2, swPeople3 })
+      })).catch(errors => {
+        // react on errors.
+      })
+})
 
 module.exports = router
